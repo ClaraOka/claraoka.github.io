e9cec12e2231a6ea10eea12d9d9699be30232963
Lucas Sifoni <lucas@lucassifoni.info>
2017-01-02 13:06:22 +0100

    Premier commit

---
+++ script.js
@@ -0,0 +1,29 @@
+(function($,win){
+
+  $(window).on('load',function(){
+    $('#projects figure').each(function(index){
+
+      var $projet = $(this);
+      var $liste = $projet.find('.liste-images');
+      $liste.css({width:$liste.find('li.active img').width()});
+      var nombreImages = $liste.find('li').length;
+
+      $(this).find('.suivant').on('click',function(e){
+        var $activeLi = $liste.find('.active');
+        var nb = $liste.find('li').index($activeLi);
+        var target = 0;
+        if(nb === nombreImages -1) {
+          target = 0;
+        } else {
+          target = nb+1;
+        }
+        $activeLi.removeClass('active');
+        var $nextLi = $liste.find('li:eq('+target+')');
+        $nextLi.addClass('active');
+        $liste.css({width:$nextLi.find('img').width()});
+      });
+
+    });
+  });
+
+})(jQuery,this);
