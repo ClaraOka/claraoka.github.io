c4e70bf139e70c57aaab62698a59743de604c463
ClaraOka <clara.choulet@gmail.com>
2018-05-11 15:42:40 +0200

    sentie-faire

---
+++ sentrefaire/ressources/script.js
@@ -0,0 +1,45 @@
+function Change() {
+
+if ((document.getElementById('iconographie').checked)) {
+  $('.icono').css("visibility","visible");
+  }
+  else {
+  $('.icono').css("visibility","hidden");
+  }
+
+if ((document.getElementById('webographie').checked)) {
+$('.webo').css("visibility","visible");
+}
+else {
+$('.webo').css("visibility","hidden");
+}
+
+if ((document.getElementById('bibliographie').checked)) {
+  $('.biblio').css("visibility","visible");
+  }
+  else {
+  $('.biblio').css("visibility","hidden");
+  }
+
+if ((document.getElementById('filmographie').checked)) {
+  $('.filmo').css("visibility","visible");
+  }
+  else {
+  $('.filmo').css("visibility","hidden");
+  }
+
+  if ((document.getElementById('poadcast').checked)) {
+    $('.poad').css("visibility","visible");
+    }
+    else {
+    $('.poad').css("visibility","hidden");
+    }
+
+if ((document.getElementById('rencontrographie').checked)) {
+  $('.rencontro').css("visibility","visible");
+  }
+  else {
+  $('.rencontro').css("visibility","hidden");
+  }
+
+}
