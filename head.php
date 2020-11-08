32329831ecf7b4ed39da9cbb40f403848d9d4e17
ClaraOka <clara.choulet@gmail.com>
2020-11-08 17:52:12 +0100

    suppr script

--- head.php
+++ head.php
@@ -3,8 +3,6 @@
 <link rel="stylesheet" href="./mobile.css" media="screen" charset="utf-8"> <!--style pour telephone mobile-->
 <link rel="icon" href="./favicon.png" >
 
-<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="script.js"></script>
 
 <meta name="p:domain_verify" content="a7e6d420c2c66117a1fd8781272392a1"/>   <!--pinterest-->
 <meta name="viewport" content="width=device-width,initial-scale=1">  <!--largeur fenetre quand diminue-->
